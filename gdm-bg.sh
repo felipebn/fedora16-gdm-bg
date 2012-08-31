a40f47b287087f65deddc7819cadc80acff12d21
Felipe Brandão <thrash.nation@gmail.com>
2012-08-31 09:59:21 -0300

    first commit

---
+++ gdm-bg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+xml=/usr/share/backgrounds/verne/default/verne.xml
+imgname=`basename $1`
+newimg="/usr/share/backgrounds/images/$imgname"
+newimg=`echo "$newimg" | awk '{gsub(/\//,"\\\/");print}'`
+
+if [ -e $1 ]; then
+	cp $1 /usr/share/backgrounds/images/
+	sed "s/\(<size.*>\)\(.*\)\(<\/size>\)/\1"$newimg"\3/g" $xml
+else
+	echo "$1: arquivo nao encontrado"
+fi
