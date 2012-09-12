f2a3a77de141d3b9778b0288cb20a188acd726e3
root <root@usr02.localdomain>
2012-09-12 08:49:47 -0300

    adding the '-i' option to sed command

--- gdm-bg.sh
+++ gdm-bg.sh
@@ -8,7 +8,7 @@ newimg=`echo "$newimg" | awk '{gsub(/\//,"\\\/");print}'`
 
 if [ -e $1 ]; then
 	cp $1 /usr/share/backgrounds/images/
-	sed "s/\(<size.*>\)\(.*\)\(<\/size>\)/\1"$newimg"\3/g" $xml
+	sed "s/\(<size.*>\)\(.*\)\(<\/size>\)/\1"$newimg"\3/g" $xml -i
 else
 	echo "$1: arquivo nao encontrado"
 fi
